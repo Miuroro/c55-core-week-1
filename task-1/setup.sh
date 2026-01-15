926f61994a2a786d91993afac9ea1b816028f8e9
Miuroro <monera.mual@gmail.com>
2026-01-15 13:03:40 +0100

    Assignment submission

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,42 @@
-# Write your code here
+#!/bin/bash
+echo "Creating project…"
+rm -rf project
+mkdir project
+cd project
+git init
+
+touch README.md
+mkdir resources
+cd resources
+touch familypicture.jpg icon.png logo.png
+cd ..
+touch settings.conf
+mkdir src
+cd src
+mkdir database
+mkdir profile
+touch program.java
+cd ..
+git add .
+git commit -m "initial commit"
+echo "Setup project…"
+echo "Welcome to my project" >> README.md
+cd src
+rmdir profile
+cd ..
+cd resources
+rm familypicture.jpg
+cd ..
+git add .
+git commit -m "deleted profile and familypicture.jpg"
+ls resources 
+echo "Setup javascript…"
+cd src
+mv program.java program.js
+echo "console.log('JavaScript works!');" >> program.js
+node program.js
+git add .
+git commit -m "changed the name of program.java to program.js and changed the content of program.js"
+cd ..
+ls ~
+echo "All done!"
